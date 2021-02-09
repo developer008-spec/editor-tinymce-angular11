4be30e156b83e282bcb11e5781641a7e19a3ccc2
Czika István <iczika@wfscorp.com>
2021-02-09 10:58:32 +0100

    adjust template variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 // https://www.tiny.cloud/docs/integrations/angular/#tinymceangularintegrationquickstartguide
 export class AppComponent {
   title = 'tinymce-angular-demo';
-  labels = ['user-name', 'user-number', 'user-email', 'user-address', 'user-phone'];
-  labelsOrder = ['order-name', 'order-date', 'order-quantity', 'order-tank', 'order-address'];
+  labels = ['order.account.firstName', 'user-number', 'user-email', 'user-address', 'user-phone'];
+  labelsOrder = ['order.deliveryNumber', 'order-date', 'order-quantity', 'order-tank', 'order-address'];
   lab = this.labels.join(' ');
   labOrder = this.labelsOrder.join(' ');
   ckeditorContent: any;
@@ -23,27 +23,8 @@ export class AppComponent {
     base_url: '/tinymce',
     suffix: '.min',
     inline: false,
-    valid_elements: '@[id|class|style|title|dir<ltr?rtl|lang|xml::lang],'
-      + 'a[rel|rev|charset|hreflang|tabindex|accesskey|type|'
-      + 'name|href|target|title|class],strong/b,em/i,strike,u,'
-      + '#p[style],-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|'
-      + 'src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,'
-      + '-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|'
-      + 'height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|'
-      + 'height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,'
-      + '#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor'
-      + '|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,'
-      + '-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face'
-      + '|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],'
-      + 'object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width'
-      + '|height|src|*],map[name],area[shape|coords|href|alt|target],bdo,'
-      + 'button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|'
-      + 'valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],'
-      + 'input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],'
-      + 'kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],'
-      + 'q[cite],samp,select[disabled|multiple|name|size],small,'
-      + 'textarea[cols|rows|disabled|name|readonly],tt,var,big',
-    valid_children: '+body[style]',
+    valid_elements: '*[*]',
+    valid_children: '*[*]',
     menu:
       {
         user: {title: 'User labels', items: 'undo redo ' + this.lab},
@@ -68,9 +49,9 @@ export class AppComponent {
         editor.ui.registry.addButton(label, {
           icon: 'template',
           tooltip: 'Insert ' + label,
-          text: 'Custom-' + label,
+          text: label,
           onAction: (_) => {
-            editor.insertContent('{{ ' + label + ' }}');
+            editor.insertContent('${ ' + label + ' }');
             component.handleEditorText();
           }
         });
@@ -78,9 +59,9 @@ export class AppComponent {
         editor.ui.registry.addMenuItem(label, {
           icon: 'user',
           tooltip: 'Insert ' + label,
-          text: 'My ' + label,
+          text: label,
           onAction: (_) => {
-            editor.insertContent('{{ ' + label + ' }}');
+            editor.insertContent('${ ' + label + ' }');
             component.handleEditorText();
           }
         });
@@ -88,11 +69,11 @@ export class AppComponent {
 
       for (const labelo of component.labelsOrder) {
         editor.ui.registry.addMenuItem(labelo, {
-          icon: 'user',
+          icon: 'template',
           tooltip: 'Insert ' + labelo,
-          text: 'My ' + labelo,
+          text: labelo,
           onAction: (_) => {
-            editor.insertContent('{{ ' + labelo + ' }}');
+            editor.insertContent('${ ' + labelo + ' }');
             component.handleEditorText();
           }
         });
@@ -104,7 +85,8 @@ export class AppComponent {
 
   handleEditorText(): void {
     console.log(this.myForm.get('body').value);
-    this.htmlContent = this.myForm.get('body').value.replaceAll('{{ user-name }}', ' John Doe ');
+    this.htmlContent = this.myForm.get('body').value.replaceAll('${ order.account.firstName }', ' John Doe ')
+      .replaceAll('${ order.deliveryNumber }', ' WP-deliveryNumber-123 ');
   }
 
 }
